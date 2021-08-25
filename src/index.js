0fda8d862f3a63ba01adf3aa39ef922b75ca92a3
İsmet Murat ONAY <34923740+muratonay1@users.noreply.github.com>
2021-08-25 13:09:23 +0300

    son

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
   res.send("İsmet Murat Onay 2021 Docker ile nodejs uygulaması");
 });
-
 app.listen(3000);
